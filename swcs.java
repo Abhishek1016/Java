4a36e2f80d3cc3f998cb70146547af9b62c7c267
Abhishek1016 <abhisinchu6@gmail.com>
2022-09-13 17:07:58 +0530

    first commit

--- swcs.java
+++ swcs.java
@@ -1,4 +1,39 @@
+import java.util.Scanner;
+
 public class swcs
 {
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter Number");
+        int val=sc.nextInt();
+        switch(val)
+        {
+            case 1:
+                System.out.println("Monday");
+                break;
 
+            case 2:
+                System.out.println("tuesday");
+                break;
+
+            case 3:
+                System.out.println("wednesday");
+                break;
+            case 4:
+                System.out.println("thursday");
+                break;
+            case 5:
+                System.out.println("friday");
+                break;
+            case 6:
+                System.out.println("saturday");
+                break;
+            case 7:
+                System.out.println("sunday");
+                break;
+            default :
+                System.out.println("Invalid number");;
+        }
+    }
 }

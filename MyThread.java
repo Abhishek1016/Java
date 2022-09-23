9dc77c45eb1da6aa39ab7752aea621bbcee03a21
Abhishek1016 <abhisinchu6@gmail.com>
2022-09-23 19:59:49 +0530

    first commit

--- MyThread.java
+++ MyThread.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class MyThread {
+class MyThread2 extends Thread
+{
+    @Override
+    public void run()
+    {
+        System.out.println(Thread.currentThread().getId()+"is running");
+    }
+}
+public class MyThread
+{
+    public static void main(String[] args)
+    {
+        for(int i=1;i<=7;i++)
+        {
+            MyThread2 th=new MyThread2();
+            th.start();
+        }
+    }
 }
